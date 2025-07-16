ae764c916fbd4331eca7234f92d08f349dac0419
dhanuzh180 <vjzaara@gmail.com>
2025-07-17 01:03:40 +0530

    Update script.js
    Signed-off-by: dhanuzh180 <vjzaara@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Birthday site loaded. Countdown + unlock features go here.");
+// Countdown timer to September 5, 12:00 AM
+const countdown = document.querySelector('.countdown');
+
+function updateCountdown() {
+    const targetDate = new Date('September 5, 2025 00:00:00').getTime();
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    if (distance <= 0) {
+        countdown.innerHTML = "🎉 It's your birthday, Partner! 🎉";
+        return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((distance % (1000 * 60)) / 1000);
+
+    countdown.innerHTML = `💖 ${days}d ${hours}h ${mins}m ${secs}s left 💖`;
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
